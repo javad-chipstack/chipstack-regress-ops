79fbcdee1f3c637ce0facddde03accf5f13403b2
javad-chipstack <159863602+javad-chipstack@users.noreply.github.com>
2025-03-30 10:07:16 -0700

    Ongoing work -- adding a checkpoint

--- app/main.pl
+++ app/main.pl
@@ -11,17 +11,15 @@ use Proc::Background;
 use File::Path qw(rmtree make_path);
 use Symbol 'gensym';
 
-$| = 1;    # Flush output immediately
-
+$| = 1;               # Flush output immediately
 my $verbosity = 0;    # Set to 1 for verbose output, 0 for silent
 
 our $CONFIG = {
     chipstack_ai_repo      => "/home/javad/dev/chipstack-ai",
     python_bin_path        => "/home/javad/.pyenv/shims/python",
     outdir                 => cwd() . "/outdir",
-    target_branch          => "main",
-    hardrestartdocker      => 1,
-    restartdocker          => 1,
+    target_branches        => "main",
+    server_restard_docker  => 1,
     design_set             => "dev_v3_mini",
     server_url             => "http://localhost:8000/",
     eda_url                => "https://eda.chipstack.ai/",
@@ -35,10 +33,10 @@ our $CONFIG = {
 };
 
 GetOptions(
-    "target_branch=s"          => \$CONFIG->{target_branch},
-    "outdir=s"                 => \$CONFIG->{outdir},
-    "hardrestartdocker!"       => \$CONFIG->{hardrestartdocker},
-    "restartdocker!"           => \$CONFIG->{restartdocker},
+    "target_branches=s"      => \$CONFIG->{target_branches},
+    "outdir=s"               => \$CONFIG->{outdir},
+    "server_restard_docker!" =>
+      \$CONFIG->{hardresserver_restard_dockertartdocker},
     "design_set=s"             => \$CONFIG->{design_set},
     "server_url=s"             => \$CONFIG->{server_url},
     "eda_url=s"                => \$CONFIG->{eda_url},
@@ -117,15 +115,25 @@ sub setup_working_directory {
 }
 
 sub restartdocker {
+    if ( !defined $CONFIG->{server_restard_docker} ) {
+        die "[ERROR] server_restard_docker is not defined.\n";
+    }
+    elsif ( $CONFIG->{server_restard_docker} !~ /^hard$|^soft$|^none$/ ) {
+        die "[ERROR] server_restard_docker must be hard, soft or none.\n";
+    }
+    elsif ( $CONFIG->{server_restard_docker} eq "none" ) {
+        print "[INFO] Skipping Docker restart.\n";
+        return;
+    }
     my ( $hardrestart, $outdir ) = @_;
     my $server_dir = $CONFIG->{chipstack_ai_repo} . "/server";
     chdir($server_dir)
       or die "[ERROR] Cannot change directory to $server_dir: $!";
-    if ($hardrestart) {
 
+    if ( $$CONFIG->{server_restard_docker} eq "hard" ) {
         run_command( "make hardrestartdocker", 12, $outdir );
     }
-    else {
+    elsif ( $$CONFIG->{server_restard_docker} eq "soft" ) {
         run_command( "make restartdocker", 12, $outdir );
     }
 
@@ -160,13 +168,12 @@ sub restartdocker {
     close $log_fh;
 }
 
-sub get_human_readable_name_for_branch {
+sub get_var_name_for_branch {
     my ($branch) = @_;
     my $human_readable_name = $branch;
     $human_readable_name =~ s/[^a-zA-Z0-9_-]/_/g;
-    $human_readable_name .= "_"
-      . join( "",
-        map { ( "a" .. "z", "A" .. "Z", 0 .. 9 )[ rand 62 ] } 1 .. 8 );
+    $human_readable_name .=
+      "_" . join( "", map { ( "a" .. "z", 0 .. 9 )[ rand 36 ] } 1 .. 10 );
     return $human_readable_name;
 }
 
@@ -179,76 +186,69 @@ sub main {
           or die "Cannot remove directory $CONFIG->{outdir}: $!";
     }
 
-    my $br_hr_name =
-      get_human_readable_name_for_branch( $CONFIG->{target_branch} );
-    my $cur_outdir = $CONFIG->{outdir} . "/$br_hr_name";
+    for my $curr_branch ( split( /[,\n\r\s]+/, $CONFIG->{target_branches} ) ) {
+        my $curr_branch_var_name = get_var_name_for_branch($curr_branch);
+        my $cur_outdir           = $CONFIG->{outdir} . "/$curr_branch_var_name";
 
-    my @commands = (
-        "gcloud auth activate-service-account "
-          . "--key-file=/home/javad/dev/chipstack-regress-ops/keys/service-account-key-kpi.json",
-        "gcloud auth print-access-token | docker login -u oauth2accesstoken "
-          . "--password-stdin https://us-west1-docker.pkg.dev",
-        "git stash push -m 'Auto-stash before reset'",
-        "git checkout main",
-        "git pull",
-        "git reset --hard origin/main",
-        "git clean -fd",
-        "git pull",
-        "git checkout $CONFIG->{target_branch}",
-        "git pull",
-        "git branch --show-current",
-        "git log -1 --oneline"
-    );
-    if ( !-d $cur_outdir ) {
-        make_path($cur_outdir)
-          or die "Cannot create log directory: $cur_outdir\n";
-    }
-    foreach my $index ( 0 .. $#commands ) {
-        my $cmd = $commands[$index];
-        run_command( $cmd, $index, $cur_outdir );
-    }
-    if ( $CONFIG->{hardrestartdocker} ) {
-        print "[INFO] Hard restarting Docker...\n";
-        restartdocker( 1, $cur_outdir );
-    }
-    elsif ( $CONFIG->{restartdocker} ) {
-        print "[INFO] Restarting Docker...\n";
-        restartdocker( 0, $cur_outdir );
-    }
-    else {
-        print "[INFO] Skipping Docker restart.\n";
-    }
-    my $python_path = join(
-        ":",
-        (
-            "$CONFIG->{chipstack_ai_repo}/common",
-            "$CONFIG->{chipstack_ai_repo}/client",
-            "$CONFIG->{chipstack_ai_repo}/kpi"
-        )
-    );
+        my @commands = (
+            "gcloud auth activate-service-account "
+              . "--key-file=/home/javad/dev/chipstack-regress-ops/keys/service-account-key-kpi.json",
+            "gcloud auth print-access-token"
+              . " | docker login -u oauth2accesstoken "
+              . "--password-stdin https://us-west1-docker.pkg.dev",
+            "git stash push -m 'Auto-stash before reset'",
+            "git checkout main",
+            "git pull",
+            "git reset --hard origin/main",
+            "git clean -fd",
+            "git pull",
+            "git checkout $curr_branch",
+            "git pull",
+            "git branch --show-current",
+            "git log -1 --oneline"
+        );
+        if ( !-d $cur_outdir ) {
+            make_path($cur_outdir)
+              or die "Cannot create log directory: $cur_outdir\n";
+        }
+        foreach my $index ( 0 .. $#commands ) {
+            my $cmd = $commands[$index];
+            run_command( $cmd, $index, $cur_outdir );
+        }
+        restartdocker( $CONFIG->{server_restard_docker}, $cur_outdir );
 
-    my $kpi_path      = "$CONFIG->{chipstack_ai_repo}/kpi/chipstack_kpi";
-    my $final_command = join(
-        " ",
-        (
-            "export PYTHONPATH=$python_path:\$PYTHONPATH ;",
-            $CONFIG->{python_bin_path},
-            "$kpi_path/app/unit_test_kpi_run.py",
-            "--design_file $kpi_path/configs/$CONFIG->{design_set}.yaml",
-            "--server_url $CONFIG->{server_url}",
-            "--eda_url $CONFIG->{eda_url}",
-            "--llm_flow $CONFIG->{llm_flow}",
-            "--syntax_check_provider $CONFIG->{syntax_check_provider}",
-            "--output_dir $cur_outdir/outdir_kpi",
-            "--enable_project_support $CONFIG->{enable_project_support}",
-            "--use_primitives $CONFIG->{use_primitives}",
-            "--iterate_simulation_results $CONFIG->{iterate_sim}",
-            "--num_random_restarts $CONFIG->{random_restarts}",
-            "--run_type $CONFIG->{run_type}"
-        )
-    );
-    run_command( $final_command, 13, $cur_outdir );
+        my $python_path = join(
+            ":",
+            (
+                "$CONFIG->{chipstack_ai_repo}/common",
+                "$CONFIG->{chipstack_ai_repo}/client",
+                "$CONFIG->{chipstack_ai_repo}/kpi"
+            )
+        );
+
+        my $kpi_path      = "$CONFIG->{chipstack_ai_repo}/kpi/chipstack_kpi";
+        my $final_command = join(
+            " ",
+            (
+                "export PYTHONPATH=$python_path:\$PYTHONPATH ;",
+                $CONFIG->{python_bin_path},
+                "$kpi_path/app/unit_test_kpi_run.py",
+                "--design_file $kpi_path/configs/$CONFIG->{design_set}.yaml",
+                "--server_url $CONFIG->{server_url}",
+                "--eda_url $CONFIG->{eda_url}",
+                "--llm_flow $CONFIG->{llm_flow}",
+                "--syntax_check_provider $CONFIG->{syntax_check_provider}",
+                "--output_dir $cur_outdir/outdir_kpi",
+                "--enable_project_support $CONFIG->{enable_project_support}",
+                "--use_primitives $CONFIG->{use_primitives}",
+                "--iterate_simulation_results $CONFIG->{iterate_sim}",
+                "--num_random_restarts $CONFIG->{random_restarts}",
+                "--run_type $CONFIG->{run_type}"
+            )
+        );
+        run_command( $final_command, 13, $cur_outdir );
+    }
+
     print "[INFO] Script execution completed successfully.\n";
 }
-
 main();
